940979358d8e33444292f18e119ea652169dcdc0
Vi <99063304@mydavinci.nl>
2020-11-24 23:49:45 +0100

    edit
    edit

--- buttons.js
+++ buttons.js
@@ -4,26 +4,52 @@ var button3 = document.getElementById('button3');
 var button0 = document.getElementById('button0');
 var img0 = document.getElementById('img0');
 
-var button1 = document.getElementById('img1k');
-var button2 = document.getElementById('img2k');
-var button3 = document.getElementById('img3k');
-var button0 = document.getElementById('img0k');
-
 button1.addEventListener('click', button1f);
 button2.addEventListener('click', button2f);
 button3.addEventListener('click', button3f);
 
+
+
+
 function button1f() {
-    img0.src = 'images/bg1.jpg';
-    img0k.src = 'images/1.jpg';
-    console.log('button1f');
-  }
-function button2f() {
-
-  }
-function button3f() {
-
-  }
-function button0f() {
+  img0.src = 'images/bg1.jpg';
+  img0k.src = 'images/1.jpg';
+  console.log('button1f');
 
 }
+function button2f() {
+  img0.src = 'images/bg2.jpg';
+  img0k.src = 'images/2.jpg';
+  console.log('button2f');
+}
+function button3f() {
+  img0.src = 'images/bg3.jpg';
+  img0k.src = 'images/3.jpg';
+  console.log('button3f');
+}
+
+// var button1 = document.getElementById('button1');
+// var button2 = document.getElementById('button2');
+// var button3 = document.getElementById('button3');
+// var button0 = document.getElementById('button0');
+// var img0 = document.getElementById('img0');
+//
+// button1.addEventListener('click', button1f);
+// button2.addEventListener('click', button2f);
+// button3.addEventListener('click', button3f);
+//
+//
+//   function button1f() {
+//
+//       img0.src = 'images/bg1.jpg';
+//
+//   }
+// function button2f() {
+//
+// }
+// function button3f() {
+//
+// }
+// function button0f() {
+//
+// }
