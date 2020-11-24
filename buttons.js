058927b3bbc6576f9f02f74055801d9ad63547a1
Vi <99063304@mydavinci.nl>
2020-11-25 00:29:45 +0100

    aanpassing

--- buttons.js
+++ buttons.js
@@ -3,30 +3,43 @@ var button2 = document.getElementById('button2');
 var button3 = document.getElementById('button3');
 var button0 = document.getElementById('button0');
 var img0 = document.getElementById('img0');
+var som = 0;
 
+button1.addEventListener('click', button1f);
 button1.addEventListener('click', button1f);
 button2.addEventListener('click', button2f);
 button3.addEventListener('click', button3f);
 
 
+function som1() {
 
+}
 
 function button1f() {
   img0.src = 'images/bg1.jpg';
   img0k.src = 'images/1.jpg';
+  som++;
+  som = document.getElementById();
+
+
+
   console.log('button1f');
 
 }
 function button2f() {
   img0.src = 'images/bg2.jpg';
   img0k.src = 'images/2.jpg';
+
   console.log('button2f');
 }
 function button3f() {
   img0.src = 'images/bg3.jpg';
   img0k.src = 'images/3.jpg';
+
   console.log('button3f');
 }
+function functionName() {
+  }
 
 // var button1 = document.getElementById('button1');
 // var button2 = document.getElementById('button2');
