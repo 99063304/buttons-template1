df57cbbc9a47bf1bc105c98e67ef0de5dfd56c13
99063304 <70692200+99063304@users.noreply.github.com>
2020-11-24 20:05:11 +0100

    Initial commit

---
+++ buttons.js
@@ -0,0 +1,24 @@
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById('button3');
+var button0 = document.getElementById('button0');
+
+button1.addEventListener('click', button1f);
+button2.addEventListener('click', button2f);
+button3.addEventListener('click', button3f);
+// button0.addEventListener('click', button0f);
+
+  function button1f() {
+
+      alert('hello');
+  
+  }
+function button2f() {
+
+}
+function button3f() {
+
+}
+function button0f() {
+
+}
